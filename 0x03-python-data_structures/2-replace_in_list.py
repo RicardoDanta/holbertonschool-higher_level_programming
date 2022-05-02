f158d16bc625bb3ac5052a446031169259a189b8
RicardoDanta <uruguaydenos@gmail.com>
2022-05-02 10:44:31 -0700

    replace

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    for i in my_list:
+        if idx < 0:
+            return my_list
+        elif idx >= len(my_list):
+            return my_list
+        else:
+            my_list[idx] = element
+            return my_list
