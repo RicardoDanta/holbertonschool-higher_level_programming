463bb8f4a303e34036feb10581e79d3ec67a10ea
RicardoDanta <uruguaydenos@gmail.com>
2022-05-04 19:31:21 -0700

    uniq

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new_list = my_list()
-    for
+    new_list = []
+    unique = set(my_list)
+    add = 0
+    for i in unique:
+        add = add + i
+    return add
---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    for i in range(len(a_dictionary)
+        if i == key:
+            pop a_dictionary[i]
+            return a_dictionary
