0b96f1aae12ce534d499d410b521bd4c00688e79
RicardoDanta <uruguaydenos@gmail.com>
2022-05-30 14:55:51 -0700

    Test

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""function that returns True if the object is an instance of a class"""
+
+
+def inherits_from(obj, a_class):
+    """otherwise False"""
+    return issubclass(list, a_class)
