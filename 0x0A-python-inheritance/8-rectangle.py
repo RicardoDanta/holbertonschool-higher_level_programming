a297eb24cd6dfc362a96cd45e16a85eeaf304c96
RicardoDanta <uruguaydenos@gmail.com>
2022-05-30 16:29:58 -0700

    commit

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -14,6 +14,7 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError("{:s} must be greater than 0".format(name))
 
+
 class Rectangle(BaseGeometry):
     """My class Rectangle"""
     def __init__(self, width, height):
