ea3e2a0a5ba0c2921eb778fbe4adbb4116be3a4f
RicardoDanta <uruguaydenos@gmail.com>
2022-05-31 12:25:31 -0700

    Read

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that reads a text file"""
+
+
+def read_file(filename=""):
+    """and prints it to stdout"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end=' ')
+    f.closed
