f7bd6afce72b79f9c45959243b14567aca3a132b
RicardoDanta <uruguaydenos@gmail.com>
2022-05-30 18:36:42 -0700

    Advanced

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class MyIn """
+
+
+class MyInt(int):
+    """ class myint """
+    def __eq__(self, result):
+        """eq"""
+
+        return False
+
+    def __ne__(self, result):
+        """ne"""
+        return True
