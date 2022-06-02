84576b7a54b650844cab29b6cdb63ef84e3b958e
RicardoDanta <uruguaydenos@gmail.com>
2022-06-01 19:41:44 -0700

    Student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Class Student"""
+
+
+class Student:
+    """that defines a student"""
+    
+    def __init__(self, first_name, last_name, age):
+        """Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """JSON"""
+        return self.__dict__
