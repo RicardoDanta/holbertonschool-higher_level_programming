1d5ab1563b2d61cfb9ac8af1e4adb131afc47185
RicardoDanta <uruguaydenos@gmail.com>
2022-07-07 14:02:11 -0700

    All

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- lists all privileges of the MySQL users on your server
-CREATE USER 'user_0d_1'@'localhost'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
