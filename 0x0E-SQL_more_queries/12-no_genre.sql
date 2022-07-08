0776c9a6b5ea8a9b92f402d84a55abb7b5e82a03
RicardoDanta <uruguaydenos@gmail.com>
2022-07-07 19:13:08 -0700

    Almost

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 -- Script that lists all shows contained in hbtn_0d_tvshows without a genre linked
-SELECT tv_shows.title, tv_show_genres.genre_id;
-FROM tv_show_genres RIGHT OUTER JOIN tv_shows;
-ON tv_show_genres.show_id = tv_shows.id WHERE tv_show_genres.show_id IS NULL;
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres RIGHT OUTER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
