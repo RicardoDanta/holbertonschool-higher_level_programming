a7da6f15ad3df604be6a3335e969cda2faa4b533
RicardoDanta <uruguaydenos@gmail.com>
2022-07-08 09:54:06 -0700

    7

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+first_3 = word[:3]
+last_2 = word[-2:]
+middle = word[1:-1]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
