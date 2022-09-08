9bd51eb94b4dcb17deb67cf36b7245f74c3c6517
RicardoDanta <uruguaydenos@gmail.com>
2022-09-08 09:53:03 -0700

    Task 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        if 'a' <= i and i >= 'z':
+        if i >= 'a' and i <= 'z':
             i = chr(ord(i) - 32)
         print("{}".format(i), end="")
-    else:
-        print(end="\n")
+    print(end="\n")
