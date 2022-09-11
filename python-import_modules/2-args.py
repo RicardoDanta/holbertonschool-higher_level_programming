b48762d678df527e56421138eb7ca469736868f7
RicardoDanta <uruguaydenos@gmail.com>
2022-09-11 16:48:47 -0700

    Task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    argc = len(argv) - 1
+    if argc == 0:
+        print(f'0 arguments.')
+    else:
+        if argc == 1:
+            print(f'{argc} argument:')
+        else:
+            print(f'{argc} arguments:')
+            for i in range(1, argc + 1):
+                print(f'{i}: {argv[i]}')
