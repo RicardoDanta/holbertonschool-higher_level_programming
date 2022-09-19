b4429bbfa974950d9084d6ef380ecdf033a05258
RicardoDanta <uruguaydenos@gmail.com>
2022-09-19 11:39:47 -0700

    Task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
+class Square():
+    """Define a Class"""
+    def __init__(self, size=0):
+        """Private instance attribute"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Define the Area"""
+        return self.__size * self.__size
