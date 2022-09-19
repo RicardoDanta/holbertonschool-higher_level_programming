e2835c520113a4676e84c996484e12b62099ab86
RicardoDanta <uruguaydenos@gmail.com>
2022-09-19 11:00:22 -0700

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
+class Square():
+    """Define a Class"""
+    def __init__(self, size):
+        """Private instance attribute"""
+        self.__size__ = size
