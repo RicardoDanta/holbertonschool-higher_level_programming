d231fe69dc2199cd7c400169a8accee3adf47f35
RicardoDanta <uruguaydenos@gmail.com>
2022-09-20 01:04:25 -0700

    Task 6

---
+++ python-classes/6-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
+class Square:
+    """Define a Class"""
+    def __init__(self, size=0, position=(0, 0)):
+        """Instantiation with optional size and optional position"""
+        self.size = size
+        self.position = position
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+        if (len(value) is not 2 or type(value) is not tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if (value[0] < 0 or value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if (type(value[0]) is not int or type(value[1]) is not int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            if self.__position[1] > 0:
+                for i in range(self.__position[1]):
+                    print("")
+            for j in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
