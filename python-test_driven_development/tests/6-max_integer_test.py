35f3ad813eb3d039f2e25c52383d5d21f2907f15
RicardoDanta <uruguaydenos@gmail.com>
2022-09-23 07:02:48 -0700

    Last Task

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -7,20 +7,21 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     """Testing the Class"""
+
     def max_integer_end(self):
-        self.assertEqual(max_integer([5, 6]), 6)
+        self.assertEqual(max_integer([5, 3, 6]), 6)
 
     def max_integer_beggining(self):
-        self.assertEqual(max_integer([6, 5]), 6)
+        self.assertEqual(max_integer([6, 5, 3]), 6)
 
     def max_integer_middle(self):
         self.assertEqual(max_integer([6, 7, 5]), 7)
 
     def negative_number(self):
-        self.assertEqual(max_integer([-5, 6]), 6)
+        self.assertEqual(max_integer([-5, 3, 6]), 6)
 
     def negative_numbers(self):
-        self.assertEqual(max_integer([-5, -6]), -5)
+        self.assertEqual(max_integer([-5, -6, -7]), -5)
 
     def list_element(self):
         self.assertEqual(max_integer([6]), 6)
