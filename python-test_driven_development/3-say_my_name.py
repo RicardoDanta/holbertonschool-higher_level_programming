bed5c6985268ea35dc857c7dc19f168ff1b63645
RicardoDanta <uruguaydenos@gmail.com>
2022-09-22 22:30:35 -0700

    Task 2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -4,8 +4,8 @@
 
 def say_my_name(first_name, last_name=""):
     """Say My Name"""
-    if first_name is not str:
+    if type(first_name) != str:
         raise TypeError("first_name must be a string")
-    if last_name is not str:
+    if type(last_name) != str:
         raise TypeError("last_name must be a string")
-    print(f"My name is <first name> <last name>")
+    print(f"My name is {first_name} {last_name}")
