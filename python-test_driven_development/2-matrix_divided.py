d1eee9840cf78f88052a0f349546c83a3a5bde6e
RicardoDanta <uruguaydenos@gmail.com>
2022-09-22 20:38:43 -0700

    Tests and Task 1

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
     """Write a function that divides all elements of a matrix"""
-    
+    if type(div) != int and type(div) != float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    m = []
+    l = len(matrix[0])
+    for j in range(len(matrix)):
+        if l != len(matrix[j]):
+            raise TypeError("Each row of the matrix must have the same size")
+        m.append([])
+        for i in matrix[j]:
+            if not isinstance(i, int) and not isinstance(i, float):
+                raise TypeError("matrix must be a matrix\
+                        (list of lists) of integers/floats")
+            m[j].append(round(i / div, 2))
+    return m
 
-    def matrix_divided(matrix, div):
-        """Matrix"""
-        if not isinstance(matrix, [[]]) or not isinstance(matrix, float):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        for row in matrix:
-            if matrix is not len(matrix):
-                raise TypeError("Each row of the matrix must have the same size")
-            if not isinstance(div, int) or not isinstance(div, float):
-                raise TypeError("div must be a number")
-            if div == 0:
-                raise ZeroDivisionError("division by zero")
-            matrix[[]] % div
-            return new_matrix
