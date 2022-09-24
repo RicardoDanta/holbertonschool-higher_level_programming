02a486a7379da2252279da4ae998c023aacd8858
RicardoDanta <uruguaydenos@gmail.com>
2022-09-24 13:44:57 -0700

    Task 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,14 +6,14 @@ class Rectangle:
     """Define a Class"""
     def __init__(self, width=0, height=0):
         """Instantiation with optional width and height"""
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
         if type(height) is not int:
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
         self.__height = height
         self.__width = width
 
