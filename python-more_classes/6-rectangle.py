a90d5d5411052321c131b1b89cfe9f5f7c97427b
RicardoDanta <uruguaydenos@gmail.com>
2022-09-25 15:45:32 -0700

    Task 6

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -18,6 +18,7 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__height = height
         self.__width = width
+        Rectangle.number_of_instances += 1
 
     def area(self):
         return self.height * self.width
@@ -40,10 +41,6 @@ class Rectangle:
 
     def __repr__(self):
         return(f"Rectangle({self.__width}, {self.__height})")
-
-    def __del__(self):
-        print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
 
     @property
     def width(self):
@@ -68,3 +65,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
