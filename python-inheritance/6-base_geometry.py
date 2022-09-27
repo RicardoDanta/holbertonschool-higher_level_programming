158d0002bf064d5cec3cee839cbfa06eb5279658
RicardoDanta <uruguaydenos@gmail.com>
2022-09-27 12:50:41 -0700

    Task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write a class BaseGeometry"""
+
+
+class BaseGeometry():
+    """Define a Class"""
+    def area(self):
+        """Exception"""
+        raise Exception("area() is not implemented")
