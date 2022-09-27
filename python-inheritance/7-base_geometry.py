20d1a45c0ab4c30fbc0ca58be20a9a53e731e8d5
RicardoDanta <uruguaydenos@gmail.com>
2022-09-27 13:00:55 -0700

    Task 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Write a class BaseGeometry"""
+
+
+class BaseGeometry():
+    """Define a Class"""
+    def area(self):
+        """Exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
+        self.name = name
+        self.value = value
