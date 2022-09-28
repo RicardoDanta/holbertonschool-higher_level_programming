3485e8a7c9688b3c68e16fc1215016299d5d9ec3
RicardoDanta <uruguaydenos@gmail.com>
2022-09-28 12:51:33 -0700

    Test 1

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Write a class Square that inherits from Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Define a Class"""
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.__size = self
+        self.integer_validator("size", size)
+
+    def area(self):
+        return self.__size * self.__size
