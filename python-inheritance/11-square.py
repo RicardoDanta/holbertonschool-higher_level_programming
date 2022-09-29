eeedb5a6a8cd8bb00639a81a5133eef869ef59b3
RicardoDanta <uruguaydenos@gmail.com>
2022-09-28 18:36:20 -0700

    Task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Write a class Square that inherits from Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Define a Class"""
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.__size = self
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        return (f"[Square] <width>/<height>")
