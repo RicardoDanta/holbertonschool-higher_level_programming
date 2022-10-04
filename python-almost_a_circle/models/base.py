11f026639039501a4d48f5f0807e90fdb1fbaf63
RicardoDanta <uruguaydenos@gmail.com>
2022-10-04 12:59:57 -0700

    Change Things

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -8,6 +8,7 @@ class Base():
     
 
     def __init__(self, id=None):
-    if type(id) is not None:
-        self.id = id
-    Base.__nb_objects += 1
+        if id is not None:
+            self.id = id
+        Base.__nb_objects += 1
+        self.id = Base.__nb_objects
