7d560223cc8d636dc14e3914071fb644fea33733
RicardoDanta <uruguaydenos@gmail.com>
2022-10-04 13:03:32 -0700

    Fixing The First

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -9,5 +9,6 @@ class Base():
     def __init__(self, id=None):
         if id is not None:
             self.id = id
-        Base.__nb_objects += 1
-        self.id = Base.__nb_objects
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
