442709636f296b7f9aa10db61d6b49a4f744bf0d
RicardoDanta <uruguaydenos@gmail.com>
2022-10-25 22:06:06 -0700

    Task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == []:
-        return None
-    my_list.sort()
-    return my_list[-1]
+    for i in range(len(my_list)):
+        if my_list[i] > my_list[0]:
+            my_list = my_list[i]
+            return my_list
