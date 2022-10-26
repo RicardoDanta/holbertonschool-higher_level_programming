11c062faa305d38998c23affb4fbf3231d4e6025
RicardoDanta <uruguaydenos@gmail.com>
2022-10-25 18:48:25 -0700

    Class to JSON

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,6 +2,7 @@
 """function that returns the dictionary description
 with simple data structure for JSON serialization of an object"""
 
+
 def class_to_json(obj):
     """Class to JSON"""
     return obj.__dict__
