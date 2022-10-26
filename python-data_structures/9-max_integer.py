cfeb04d266e578379afe4c0ee4e395576b7b97c7
RicardoDanta <uruguaydenos@gmail.com>
2022-10-26 11:11:09 -0700

    Change some things

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     number = my_list[0]
-    if my_list == []:
-        return None
     for i in range(len(my_list)):
         if my_list[i] > number:
             number = my_list[i]
