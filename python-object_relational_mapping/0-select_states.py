165912217205af5f73f7fd6df0cb9c16d0a3fd21
RicardoDanta <uruguaydenos@gmail.com>
2022-11-12 14:23:49 -0800

    Task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,9 +8,9 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
                          port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
 
     cur = db.cursor()
 
