849f25e4aa9e883691f75304aad344e9a57d501a
RicardoDanta <uruguaydenos@gmail.com>
2022-11-12 18:33:23 -0800

    Fixing Pycodestyle

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""write a script that takes in arguments and displays all values in the states table
+"""write a script that takes in arguments and displays
+all values in the states table
 of hbtn_0e_0_usa where name matches the argument.
 But this time, write one that is safe from MySQL injections!"""
 
