45964cd1572432c6351ef803ce57b124b6649da2
RicardoDanta <uruguaydenos@gmail.com>
2022-11-13 21:30:33 -0800

    Task 10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Write a script that lists all State objects that\
+"""Write a script that lists all State objects that
 contain the letter a from the database hbtn_0e_6_usa"""
 
 import sys
