655dba444ecb35d933cef6704c1671c15fcad828
RicardoDanta <uruguaydenos@gmail.com>
2022-11-14 11:55:22 -0800

    Task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for i in row:
-            print("{:d}".format(i))
+    for x in matrix:
+        print(*x)
