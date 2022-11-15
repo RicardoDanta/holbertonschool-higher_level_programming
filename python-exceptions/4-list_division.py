648d7594a5988baffd767c246a558fb6e0d9ca1f
RicardoDanta <uruguaydenos@gmail.com>
2022-11-15 11:36:29 -0800

    Task 4

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(list_length):
+        try:
+            total = my_list_1[i] / my_list_2[i]
+        except (ValueError, TypeError):
+            print("wrong type")
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        finally:
+            new_list.append(total)
+    return new_list
