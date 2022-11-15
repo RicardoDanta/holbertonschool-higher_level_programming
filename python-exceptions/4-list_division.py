7087ea049336732f89b20c1ca3caf6556b8ea495
RicardoDanta <uruguaydenos@gmail.com>
2022-11-15 11:37:51 -0800

    Task 4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -6,10 +6,13 @@ def list_division(my_list_1, my_list_2, list_length):
             total = my_list_1[i] / my_list_2[i]
         except (ValueError, TypeError):
             print("wrong type")
+            total = 0
         except ZeroDivisionError:
             print("division by 0")
+            total = 0
         except IndexError:
             print("out of range")
+            total = 0
         finally:
             new_list.append(total)
     return new_list
