e1c6dff12140076877e68f7f3fcb1523135f1164
RicardoDanta <uruguaydenos@gmail.com>
2022-11-15 21:29:57 -0800

    Task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""class Student that defines a student"""
+
+
+class Student():
+    """Define a Class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """JSON"""
+        if attrs is None:
+            return self.__dict__
+        dictionary = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                dictionary[key] = value
+        return dictionary
