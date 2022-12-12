a62978a9ba5c213e017629eaa1fec38cde36fcd1
RicardoDanta <uruguaydenos@gmail.com>
2022-12-11 17:38:37 -0800

    Task 7

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const c = 'C is fun';
+const argv = process.argv;
+if (!argv[2]) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < argv[2]; i++)
+	{ console.log(c); }
+}
