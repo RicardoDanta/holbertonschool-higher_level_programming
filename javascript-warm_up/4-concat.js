94cd38e6c1e89c97466be0560ed72d6362431ba0
RicardoDanta <uruguaydenos@gmail.com>
2022-12-11 16:12:55 -0800

    Fixing Semistandard

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -2,7 +2,7 @@
 
 const argv = process.argv;
 if (argv[3]) {
-	console.log(argv[2] + ' is ' + argv[3]);
+  console.log(argv[2] + ' is ' + argv[3]);
 } else {
-	console.log(argv[2] + ' is ' + argv[3]);
+  console.log(argv[2] + ' is ' + argv[3]);
 }
