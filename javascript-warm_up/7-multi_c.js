9a4732aaebb3fcaa279d6f2ce62a29ae91920210
RicardoDanta <uruguaydenos@gmail.com>
2022-12-11 17:40:20 -0800

    Fixing Semistandard!

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -5,6 +5,5 @@ const argv = process.argv;
 if (!argv[2]) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < argv[2]; i++)
-	{ console.log(c); }
+  for (let i = 0; i < argv[2]; i++) { console.log(c); }
 }
