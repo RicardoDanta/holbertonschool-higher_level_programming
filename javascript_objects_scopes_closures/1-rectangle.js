4b995a117b436d84fbc3f5dacc44e8b5338f8c8e
RicardoDanta <uruguaydenos@gmail.com>
2022-12-16 09:53:54 -0800

    Change W and H!

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-  constructor (w, h) {
-    this.height = h;
-    this.width = w;
-  }
-};
+	constructor (w, h) {
+		this.width = w;
+		this.height = h;
+	}
+}
