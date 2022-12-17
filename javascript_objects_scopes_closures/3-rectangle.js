e819b67b0e32fc7cf87761a1806cd16e7c9fabe2
RicardoDanta <uruguaydenos@gmail.com>
2022-12-17 04:31:48 -0800

    Task 3

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+	  const Rectangle = new print();
+	const equis = 'X';
+	console.log(equis);
+}
