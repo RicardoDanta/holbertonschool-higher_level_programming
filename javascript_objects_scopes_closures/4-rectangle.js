b54350eebbbc6dcbe96be3ee5bc67a3bb8b73614
RicardoDanta <uruguaydenos@gmail.com>
2023-01-13 12:17:03 -0800

    Task 4

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const x = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(x.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const i = this.width;
+    this.width = this.height;
+    this.height = i;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
