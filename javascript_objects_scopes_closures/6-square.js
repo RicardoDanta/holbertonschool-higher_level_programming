9722ceab2631a514d1c94400d5f15c5d4922bf5d
RicardoDanta <uruguaydenos@gmail.com>
2023-01-13 18:48:36 -0800

    Fixing the 6th!

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-module.exports = class Square extends require('./4-rectangle.js') {
+module.exports = class Square extends require('./5-square.js') {
   constructor (size) {
     super(size, size);
   }
