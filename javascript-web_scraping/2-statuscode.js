78477eba887603ed7cc331477478deec3178a5cb
RicardoDanta <uruguaydenos@gmail.com>
2023-01-22 10:55:35 -0800

    Task 2!

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -2,10 +2,10 @@
 
 const request = require('request');
 
-request.get(process.argv[2], function (err, response) {
+request(process.argv[2], function (err, response) {
   if (err) {
     console.log(err);
   } else {
-    console.log('code: ', response.statusCode);
+    console.log('code: ', response && response.statusCode);
   }
 });
