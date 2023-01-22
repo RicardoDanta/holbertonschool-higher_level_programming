d0e0d6dba6b40c09c4a7a4aeccb8a361c373d18d
RicardoDanta <uruguaydenos@gmail.com>
2023-01-22 10:41:55 -0800

    Try something!

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request.get(process.argv[2], function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ', response.statusCode);
+  }
+});
