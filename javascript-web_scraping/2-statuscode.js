6638ed4347226193b115004434c5c1fc195b9c6c
RicardoDanta <uruguaydenos@gmail.com>
2023-01-22 11:00:12 -0800

    Add Get!

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 
-request(process.argv[2], function (err, response) {
+request.get(process.argv[2], function (err, response) {
   if (err) {
     console.log(err);
   } else {
