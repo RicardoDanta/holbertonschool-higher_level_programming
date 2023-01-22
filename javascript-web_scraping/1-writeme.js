7ceffa2099bb1eeebc4f99feb82a60109a901a2c
RicardoDanta <uruguaydenos@gmail.com>
2023-01-22 10:13:42 -0800

    Delete another thing!

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err, data) {
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err) {
   if (err) {
     console.log(err);
   }
