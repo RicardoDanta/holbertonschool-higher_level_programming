7c4c6622a908809db40b90d3b00f8dc542eec46f
RicardoDanta <uruguaydenos@gmail.com>
2023-01-22 10:12:03 -0800

    Delete a line!

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -5,7 +5,5 @@ const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
-  } else {
-    console.log(data);
   }
 });
