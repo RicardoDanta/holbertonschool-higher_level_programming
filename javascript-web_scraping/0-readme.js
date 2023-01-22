77dda972ff64d4d7f4f8ee6bcee6ab14c90e8987
RicardoDanta <uruguaydenos@gmail.com>
2023-01-22 06:48:58 -0800

    Semistandard Validation!

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,10 +2,10 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], "utf-8", function (err, data) {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(data);
-	}
+fs.readFile(process.argv[2], 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
