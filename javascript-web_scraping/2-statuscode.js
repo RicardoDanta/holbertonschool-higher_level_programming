37f6add157fb6e08d16dbfd2dc516d25e748d90f
RicardoDanta <uruguaydenos@gmail.com>
2023-01-22 11:01:44 -0800

    Delete a Response!

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -6,6 +6,6 @@ request.get(process.argv[2], function (err, response) {
   if (err) {
     console.log(err);
   } else {
-    console.log('code: ', response && response.statusCode);
+    console.log('code: ', response.statusCode);
   }
 });
