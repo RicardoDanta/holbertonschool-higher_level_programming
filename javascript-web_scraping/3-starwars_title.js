39a0278a683fe20274087cc0c66734bd44b8c039
RicardoDanta <uruguaydenos@gmail.com>
2023-01-23 08:22:33 -0800

    Task 3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, response, body) {
+  if (err) {
+	  console.log(err);
+  } else {
+	  console.log(JSON.parse(body));
+  }
+});
