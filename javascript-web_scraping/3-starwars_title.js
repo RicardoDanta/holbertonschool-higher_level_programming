ca8d9365389247073ca68be2908890e22c7f6225
RicardoDanta <uruguaydenos@gmail.com>
2023-01-23 08:25:14 -0800

    Semistandard Validation!

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -4,8 +4,8 @@ const request = require('request');
 
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, response, body) {
   if (err) {
-	  console.log(err);
+    console.log(err);
   } else {
-	  console.log(JSON.parse(body));
+    console.log(JSON.parse(body));
   }
 });
